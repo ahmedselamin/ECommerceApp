f4eef27aa539bdf8fb125073d3e4e832d3eb0566
eddyiiz <eddyxsayf@gmail.com>
2024-06-07 07:30:02 +0200

    display price on client

--- EcommerceApp/Server/Services/ProductService/ProductService.cs
+++ EcommerceApp/Server/Services/ProductService/ProductService.cs
@@ -14,7 +14,7 @@ namespace EcommerceApp.Server.Services.ProductService
         {
             var response = new ServiceResponse<List<Product>>
             {
-                Data = await _context.Products.ToListAsync()
+                Data = await _context.Products.Include(p => p.Variants).ToListAsync()
             };
 
             return response;
@@ -47,6 +47,7 @@ namespace EcommerceApp.Server.Services.ProductService
             {
                 Data = await _context.Products
                             .Where(p => p.Category.Url.ToLower().Equals(categoryUrl.ToLower()))
+                            .Include(p => p.Variants)
                             .ToListAsync()
             };
 
