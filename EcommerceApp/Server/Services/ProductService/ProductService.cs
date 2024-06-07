3d84c493a822477966d79b520b80c4d2a342d300
eddyiiz <eddyxsayf@gmail.com>
2024-06-07 06:24:35 +0200

    Get the products with variants and types

--- EcommerceApp/Server/Services/ProductService/ProductService.cs
+++ EcommerceApp/Server/Services/ProductService/ProductService.cs
@@ -24,7 +24,10 @@ namespace EcommerceApp.Server.Services.ProductService
         {
             var response = new ServiceResponse<Product>();
 
-            var product = await _context.Products.FindAsync(productId);
+            var product = await _context.Products
+                .Include(p => p.Variants)
+                .ThenInclude(v => v.ProductType)
+                .FirstOrDefaultAsync(p => p.Id == productId);
             if (product == null)
             {
                 response.Success = false;
